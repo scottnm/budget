f09cd7b9e24e3baa9fde5b2e23f7430e822d715c
Scott Munro <scottnmunro@gmail.com>
2018-07-03 22:52:11 -0700

    added simple arg validation and readline the lines from the csv file

--- Budget.py
+++ Budget.py
@@ -1,6 +1,11 @@
 import pickle
+import sys
 
 CACHE_FILE_NAME = 'CategoryCache.data'
+
+def PrintUsageError(errorString):
+    print('Error: {}'.format(errorString))
+    print('Usage: python Budget.py BankStatement.csv')
 
 def LoadCache(cacheFileName):
     try:
@@ -14,17 +19,31 @@ def LoadCache(cacheFileName):
 
 def SaveCache(cacheFileName, categoryCache):
     with open(cacheFileName, 'wb+') as cacheFile:
+        # print results
         print('Saving cache to {}: {}'.format(cacheFileName, categoryCache))
         pickle.dump(categoryCache, cacheFile)
 
 def Main():
+    # Verify Args
+    if len(sys.argv) < 2:
+        PrintUsageError('Not enough args!')
+        return
+
+    bankStatementFileName = sys.argv[1]
+    if not bankStatementFileName.endswith('.csv'):
+        PrintUsageError('File to parse must be csv')
+        return
+
     # load the file containing the old categories and dict that maps recurring descriptions to categories
     categoryCache = LoadCache(CACHE_FILE_NAME)
+
     # load the csv file from chase
-    # read each line for amount and description
-        # if description auto matches, tally it up
-        # if it doesn't prompt user to categorize it
-    # print results
+    with open(bankStatementFileName, 'r') as bankStatementFile:
+        for line in bankStatementFile.readlines():
+            print(line)
+        # read each line for amount and description
+            # if description auto matches, tally it up
+            # if it doesn't prompt user to categorize it
     # save the file containing the old categories and dict that maps recurring descriptions to categories
     SaveCache(CACHE_FILE_NAME, categoryCache)
 
