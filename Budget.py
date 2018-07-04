f0505ceed122a148acc1e63c4632f1123c8f1348
Scott Munro <scottnmunro@gmail.com>
2018-07-04 00:17:21 -0700

    auto categorizing by substring and reporting

--- Budget.py
+++ Budget.py
@@ -4,6 +4,11 @@ import sys
 
 CACHE_FILE_NAME = 'CategoryCache.data'
 
+class CategoryCacheEntry:
+    def __init__(self, descriptionMatch, category):
+        self.descriptionMatch = descriptionMatch
+        self.category = category
+
 def PrintUsageError(errorString):
     print('Error: {}'.format(errorString))
     print('Usage: python Budget.py BankStatement.csv')
@@ -12,7 +17,7 @@ def LoadCache(cacheFileName):
     try:
         with open(cacheFileName, 'rb') as cacheFile:
             cache = pickle.load(cacheFile)
-            print('Loaded cache from {}: {}'.format(cacheFileName, cache))
+            print('Loaded cache from {}'.format(cacheFileName))
             return cache
     except FileNotFoundError:
         print('No cache file found @ {}'.format(cacheFileName))
@@ -21,8 +26,19 @@ def LoadCache(cacheFileName):
 def SaveCache(cacheFileName, categoryCache):
     with open(cacheFileName, 'wb+') as cacheFile:
         # print results
-        print('Saving cache to {}: {}'.format(cacheFileName, categoryCache))
+        print('Saving cache to {}'.format(cacheFileName))
         pickle.dump(categoryCache, cacheFile)
+
+def FindCategoryFromCache(categoryCache, description):
+    for cacheEntry in categoryCache:
+        if cacheEntry.descriptionMatch.lower() in description.lower():
+            return cacheEntry.category
+    return None
+
+def AssignCategory(categoryCache, description):
+    category = input('Enter category for "{}":'.format(description))
+    categoryCache.append(CategoryCacheEntry(description, category))
+    return category
 
 def Main():
     # Verify Args
@@ -38,24 +54,42 @@ def Main():
     # load the file containing the old categories and dict that maps recurring descriptions to categories
     categoryCache = LoadCache(CACHE_FILE_NAME)
 
+    debitReport = dict()
+
     # load the csv file from chase
     with open(bankStatementFileName, 'r') as bankStatementFile:
         debitLines = [line for line in bankStatementFile.readlines() if line.lower().startswith('debit')]
         transactionLine = re.compile(r'\w+,\d{2}/\d{2}/\d{4},"(.*)\s+\d{2}/\d{2}",(-?\d+.\d+)')
+        # read each line for amount and description
         for line in debitLines:
             match = transactionLine.match(line)
             if match is None:
                 continue
             assert len(match.groups()) == 2
-            description = match.group(1)
+            description = re.sub(r'\s+', ' ', match.group(1))
             amount = float(match.group(2))
             print('amount: {}'.format(amount))
             print('description: {}'.format(description))
             print()
 
-            # read each line for amount and description
-                # if description auto matches, tally it up
+            # if description auto matches, tally it up
+            category = FindCategoryFromCache(categoryCache, description)
+            if category is None:
                 # if it doesn't prompt user to categorize it
+                category = AssignCategory(categoryCache, description)
+
+            if category not in debitReport:
+                debitReport[category] = 0
+            debitReport[category] += amount
+
+    # flatten the debit report map into a sorted list
+    debitReportList = sorted(
+        [(category, categoryDebitSum) for category, categoryDebitSum in debitReport.items()],
+        key=lambda debitReportListEntry: debitReportListEntry[1])
+
+    for debitReportListEntry in debitReportList:
+        print('You spent ${:.2f} on {}'.format(-1 * debitReportListEntry[1], debitReportListEntry[0]))
+
     # save the file containing the old categories and dict that maps recurring descriptions to categories
     SaveCache(CACHE_FILE_NAME, categoryCache)
 
