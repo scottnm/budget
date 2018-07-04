94e832c695aa7da3118f85830c28ac45ef46b282
Scott Munro <scottnmunro@gmail.com>
2018-07-03 22:09:29 -0700

    added plan

--- Budget.py
+++ Budget.py
@@ -1,5 +1,13 @@
 def Main():
+    # load the file containing the old categories and dict that maps recurring descriptions to categories
+    # load the csv file from chase
+    # read each line for amount and description
+        # if description auto matches, tally it up
+        # if it doesn't prompt user to categorize it
+    # print results
+    # save the file containing the old categories and dict that maps recurring descriptions to categories
     print("Main")
+    pass
 
 if __name__ == '__main__':
     Main()
