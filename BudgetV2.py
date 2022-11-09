b2805de218211379673e5990cac2e03af8722f13
Scott Munro <scottnmunro@gmail.com>
2022-11-09 00:20:40 -0800

    added the beginnings of an interactive mode

--- BudgetV2.py
+++ BudgetV2.py
@@ -7,6 +7,33 @@ import pickle
 import pprint
 import re
 import sys
+import tkinter
+from tkinter import filedialog
+
+class FileDialog:
+    root = None
+
+    def __EnsureInitialized():
+        if FileDialog.root is None:
+            FileDialog.root = tkinter.Tk()
+            FileDialog.root.withdraw()
+
+    def Open():
+        FileDialog.__EnsureInitialized()
+
+        fname = tkinter.filedialog.askopenfilename()
+        if fname == '':
+            fname = None
+        return fname
+
+    def AskSaveAs(initial_name):
+        FileDialog.__EnsureInitialized()
+
+        fname = tkinter.filedialog.asksaveasfile(initialfile=initial_name)
+
+        if fname == '':
+            fname = None
+        return fname
 
 class Account(enum.Enum):
     Checking = 0
@@ -57,18 +84,21 @@ class Transaction:
             self.datetime)
 
 class TransactionDb:
-    def __init__(self, transactions):
+    def __init__(self, transactions=set()):
         self.transactions = transactions
 
-    def Load(file_name):
+    def Load(file_name, default_on_missing=False):
         try:
             with open(file_name, 'rb') as file:
                 transaction_db = pickle.load(file)
                 print('Loaded transaction DB from {}'.format(file_name))
                 return transaction_db
         except FileNotFoundError as e:
-            print('Transaction DB file {} does not exist. Starting new empty DB.'.format(file_name))
-            return TransactionDb(set())
+            if default_on_missing:
+                print('Transaction DB file {} does not exist. Starting new empty DB.'.format(file_name))
+                return TransactionDb()
+            else:
+                raise e
         except BaseException as e:
             print('Could not load transaction DB from {}! {}'.format(file_name, e))
             raise e
@@ -85,7 +115,111 @@ def PrintUsageError(errorString):
     print('Error: {}'.format(errorString))
     print('Usage: python BudgetV2.py BankStatement.csv TransactionDb.data')
 
-def main():
+class InteractiveModeState:
+    def __init__(self):
+        self.loaded_db = None
+
+class InteractiveMode(enum.Enum):
+    Quit = 0
+    MainMenu = 1
+    NewDb = 2
+    LoadDb = 3
+    SaveDb = 4
+
+
+def present_prompt(prompt, option_dict):
+    while True:
+        if prompt is not None:
+            print(prompt)
+        for k,v in option_dict.items():
+            print("[%s] - %s" % (k, v[0]))
+
+        selection = input()
+        if selection in option_dict:
+            return option_dict[selection][1]
+        if selection.lower() in option_dict:
+            return option_dict[selection.lower()][1]
+        if selection.upper() in option_dict:
+            return option_dict[selection.upper()][1]
+        else:
+            print("Unrecognized input %s" % selection.lower())
+
+def load_db_interactive():
+    db_file_name = FileDialog.Open()
+    if db_file_name is not None:
+        try:
+            return (db_file_name, TransactionDb.Load(db_file_name, default_on_missing=True))
+        except BaseException as e:
+            print("Failed to open db file: %s" % e)
+            return None
+    else:
+        return None
+
+
+def main_interactive():
+    mode = InteractiveMode.MainMenu
+    state = InteractiveModeState()
+
+    while mode != InteractiveMode.Quit:
+        match mode:
+            case InteractiveMode.MainMenu:
+                print()
+                mode = present_prompt(None, {
+                    'NDB': ("new db", InteractiveMode.NewDb),
+                    'LDB': ("load db", InteractiveMode.LoadDb),
+                    'SDB': ("save db", InteractiveMode.SaveDb),
+                    'Q': ("quit", InteractiveMode.Quit),
+                    })
+
+            case InteractiveMode.NewDb:
+                if state.loaded_db is None:
+                    print("Loaded new DB.")
+                    state.loaded_db = (None, TransactionDb())
+                else:
+                    # FIXME: support save on new db
+                    print("ERROR: already loaded DB")
+                mode = InteractiveMode.MainMenu
+
+            case InteractiveMode.LoadDb:
+                if state.loaded_db is None:
+                    db = load_db_interactive()
+                    if db is not None:
+                        state.loaded_db = db
+                        print("Loaded db. %s (%i entries)" % (
+                            state.loaded_db[0],
+                            len(state.loaded_db[1].transactions)
+                            ))
+                else:
+                    # FIXME: support save on re-load
+                    print("ERROR: already loaded DB")
+                mode = InteractiveMode.MainMenu
+
+            case InteractiveMode.SaveDb:
+                if state.loaded_db is not None:
+                    save_file_name = None
+                    if state.loaded_db[0] is not None:
+                        save_file_name = state.loaded_db[0]
+                    else:
+                        save_file = FileDialog.AskSaveAs("budget.db")
+                        save_file_name = save_file.name
+                        save_file.close()
+
+                    if save_file_name is not None:
+                        print("Saving db. %s (%i entries)" % (
+                            save_file_name,
+                            len(state.loaded_db[1].transactions)
+                            ))
+                        state.loaded_db[1].Save(save_file_name)
+                        state.loaded_db = (save_file_name, state.loaded_db[1])
+                else:
+                    # FIXME: support save on re-load
+                    print("ERROR: DB not loaded")
+                mode = InteractiveMode.MainMenu
+
+            case _:
+                break
+
+def main_cli():
     # verify args
     if len(sys.argv) < 3:
         PrintUsageError('Not enough args!')
@@ -97,7 +231,7 @@ def main():
         return
 
     db_file_name = sys.argv[2]
-    transaction_db = TransactionDb.Load(db_file_name)
+    transaction_db = TransactionDb.Load(db_file_name, default_on_missing=True)
     print('Loaded transaction db with {} entries'.format(len(transaction_db.transactions)))
 
     with open(bank_statement_file_name) as bank_statement_csv:
@@ -109,6 +243,12 @@ def main():
     if len(sys.argv) >= 4:
         print('Saving transaction db with {} entries'.format(len(transaction_db.transactions)))
         transaction_db.Save(db_file_name)
+
+def main():
+    if "-i" in sys.argv:
+        main_interactive()
+    else:
+        main_cli()
 
 if __name__ == '__main__':
     main()
